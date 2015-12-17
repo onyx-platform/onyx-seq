0549b6b3db1c0d49aa21b64b4d7fde463cc15d21
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-12-18 04:17:29 +0800

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-seq "0.8.3.1"
+(defproject org.onyxplatform/onyx-seq "0.8.3.2-SNAPSHOT"
   :description "Onyx plugin for seq"
   :url "https://github.com/onyx-platform/onyq-seq"
   :license {:name "Eclipse Public License"
