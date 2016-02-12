0c9da489449bdc89f656d58fd74871e60bf38266
OnyxBot <michael.drogalis@onyxplatform.org>
2016-02-12 13:25:41 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-seq "0.8.10.0-alpha2"
+(defproject org.onyxplatform/onyx-seq "0.8.10.0-SNAPSHOT"
   :description "Onyx plugin for seq"
   :url "https://github.com/onyx-platform/onyq-seq"
   :license {:name "Eclipse Public License"
