c9d0ea202522fccab7a160a6224ef16617e049e9
OnyxBot <michael.drogalis@onyxplatform.org>
2016-03-08 13:39:34 +0000

    Release version 0.9.0.0-alpha9.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-seq "0.9.0.0-SNAPSHOT"
+(defproject org.onyxplatform/onyx-seq "0.9.0.0-alpha9"
   :description "Onyx plugin for seq"
   :url "https://github.com/onyx-platform/onyq-seq"
   :license {:name "Eclipse Public License"
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.9.0-alpha6"]]
+                 [org.onyxplatform/onyx "0.9.0-alpha9"]]
   :profiles {:dev {:plugins [[lein-set-version "0.4.1"]
                              [lein-update-dependency "0.1.2"]
                              [lein-pprint "1.1.1"]]}
